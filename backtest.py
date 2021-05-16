1c57b23f507d0d9dc234dd4845109e804a24bc4d
andrenaumann94 <59823795+andrenaumann94@users.noreply.github.com>
2021-05-16 14:37:24 +0200

    backtest testing

--- backtest.py
+++ backtest.py
@@ -4,12 +4,12 @@ from binance_trade_bot import backtest
 
 if __name__ == "__main__":
     history = []
-    for manager in backtest(datetime(2021, 1, 1), datetime.now()):
+    for manager in backtest(datetime(2020, 4, 1), datetime(2020, 5, 1)):  # datetime.now()):
         btc_value = manager.collate_coins("BTC")
         bridge_value = manager.collate_coins(manager.config.BRIDGE.symbol)
         history.append((btc_value, bridge_value))
-        btc_diff = round((btc_value - history[0][0]) / history[0][0] * 100, 3)
-        bridge_diff = round((bridge_value - history[0][1]) / history[0][1] * 100, 3)
+        btc_diff = round((btc_value - history[0][0]) / history[0][0] * 20, 3)
+        bridge_diff = round((bridge_value - history[0][1]) / history[0][1] * 20, 3)
         print("------")
         print("TIME:", manager.datetime)
         print("BALANCES:", manager.balances)
